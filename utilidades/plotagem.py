e910b2621ae12a7dbd6ee27e1cb34684d6590b20
Otávio Henrique de Freitas Rabelo <otaviorabelounb@gmail.com>
2021-12-03 23:54:56 -0300

    Add files via upload

---
+++ utilidades/plotagem.py
@@ -0,0 +1,23 @@
+# Este módulo pertence ao pacote 'utilidades'
+# e é utilizado para a plotagem dos gráficos
+# requeridos pelo algoritmo.
+
+from matplotlib.pyplot import (pie, legend, title, axis, show)
+
+def pizza(lista):
+    """Função utilizada para plotar
+    um gráfico com a distribuição dos
+    cogumelos após a analise do
+    algoritmo."""
+    dicionario = {'Comestíveis': 0, 'Venenosos': 0}
+    for rotulo in lista:
+        if rotulo == 'e':
+            dicionario['Comestíveis'] += 1
+        else:
+            dicionario['Venenosos'] += 1
+    pie(dicionario.values(), labels=dicionario.keys(), autopct='%1.2f%%', startangle=90)
+    legend(title='Legenda:', shadow=True, loc='best')
+    title("Distribuição das classes dos cogumelos")
+    axis("equal")
+    show()
+    return 0
